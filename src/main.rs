2b1cd000b5bb85ebbd52557aa20ac0ba5c245161
brndd <burneddi@gmail.com>
2022-07-09 02:38:39 +0300

    Pass by reference rather than move

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ use std::ffi::OsString;
  * Replaces the first occurrence of find with replace, mutating haystack.
  * Returns true if a replacement occurred, false if not.
  */
-fn replace(haystack: &mut Vec<u8>, find: Vec<u8>, replace: Vec<u8>) -> bool {
+fn replace(haystack: &mut Vec<u8>, find: &Vec<u8>, replace: &Vec<u8>) -> bool {
     if haystack.len() < find.len() {
         return false;
     }
@@ -35,7 +35,7 @@ fn replace(haystack: &mut Vec<u8>, find: Vec<u8>, replace: Vec<u8>) -> bool {
         Some(idx) => idx
     };
 
-    haystack.splice(match_index..match_index+replace.len(), replace);
+    haystack.splice(match_index..match_index+replace.len(), replace.iter().cloned());
     return true;
 }
 
@@ -103,7 +103,7 @@ Usage: {cmd} path/to/WoW.exe");
     let widescreen_fov_find: Vec::<u8> = vec![0xDB, 0x0F, 0xC9, 0x3F, 0xE6, 0xF1, 0x47, 0x40, 0x00, 0x00];
     let widescreen_fov_repl: Vec::<u8> = vec![0x66, 0x66, 0xF6, 0x3F, 0xE6, 0xF1, 0x47, 0x40, 0x00, 0x00];
     print!("Applying patch: widescreen FoV fix...");
-    match replace(&mut file, widescreen_fov_find, widescreen_fov_repl) {
+    match replace(&mut file, &widescreen_fov_find, &widescreen_fov_repl) {
         true => println!(" Success!"),
         false => {
             println!(" FAILED!");
@@ -116,7 +116,7 @@ Usage: {cmd} path/to/WoW.exe");
     let sound_in_background_repl: Vec::<u8> = vec![0x85, 0xc9, 0x74, 0x2d, 0x8b, 0x01, 0x85, 0xc0, 0x74, 0x27, 0x6a, 0x00, 0x6a, 0xfd, 0xa3, 0xb0, 0x55, 0xcf, 0x00, 0xe8, 0xc0, 0x92, 0x05, 0x00, 0xb8, 0x01, 0x00, 0x00, 0x00, 0xc3, 0x6a, 0x01, 0x6a, 0xfd, 0xc7, 0x05, 0xb0, 0x55, 0xcf, 0x00, 0x00, 0x00, 0x00, 0x00, 0xe8, 0xa7, 0x92, 0x05, 0x00, 0xb8, 0x01, 0x00, 0x00, 0x00, 0xc3];
 
     print!("Applying patch: sound in background...");
-    match replace(&mut file, sound_in_background_find, sound_in_background_repl) {
+    match replace(&mut file, &sound_in_background_find, &sound_in_background_repl) {
         true => println!(" Success!"),
         false => {
             println!(" FAILED!");
@@ -138,7 +138,7 @@ Usage: {cmd} path/to/WoW.exe");
     let quickloot_reverse_repl_corpse: Vec::<u8> = vec![0x04, 0x3b, 0x15, 0x9c, 0xda, 0xc4, 0x00, 0x75, 0x10, 0x8b, 0x53, 0x08, 0x85, 0xd2, 0x0f, 0x94, 0xc0, 0x8b, 0xce, 0x50, 0xe8, 0x77, 0x34, 0xfd, 0xff, 0x8b, 0xce, 0xe8, 0x10, 0x39, 0x00, 0x00];
     
     print!("Applying patch: quickloot reverse (world objects)...");
-    match replace(&mut file, quickloot_reverse_find_fish, quickloot_reverse_repl_fish) {
+    match replace(&mut file, &quickloot_reverse_find_fish, &quickloot_reverse_repl_fish) {
         true => println!(" Success!"),
         false => {
             println!(" FAILED!");
@@ -147,7 +147,7 @@ Usage: {cmd} path/to/WoW.exe");
     }
 
     print!("Applying patch: quickloot reverse (container items)...");
-    match replace(&mut file, quickloot_reverse_find_containeritem, quickloot_reverse_repl_containeritem) {
+    match replace(&mut file, &quickloot_reverse_find_containeritem, &quickloot_reverse_repl_containeritem) {
         true => println!(" Success!"),
         false => {
             println!(" FAILED!");
@@ -156,7 +156,7 @@ Usage: {cmd} path/to/WoW.exe");
     }
 
     print!("Applying patch: quickloot reverse (corpses)...");
-    match replace(&mut file, quickloot_reverse_find_corpse, quickloot_reverse_repl_corpse) {
+    match replace(&mut file, &quickloot_reverse_find_corpse, &quickloot_reverse_repl_corpse) {
         true => println!(" Success!"),
         false => {
             println!(" FAILED!");
@@ -194,7 +194,7 @@ mod tests {
         let mut data: Vec::<u8> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
         let find: Vec::<u8> = vec![3, 4, 5, 6];
         let repl: Vec::<u8> = vec![6, 5, 4, 3];
-        let return_val = replace(&mut data, find, repl);
+        let return_val = replace(&mut data, &find, &repl);
         assert_eq!(data, [1u8, 2, 6, 5, 4, 3, 7, 8, 9, 10]);
         assert!(return_val);
     }
@@ -204,7 +204,7 @@ mod tests {
         let mut data: Vec::<u8> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
         let find: Vec::<u8> = vec![6, 6, 6, 6];
         let repl: Vec::<u8> = vec![6, 5, 4, 3];
-        let return_val = replace(&mut data, find, repl);
+        let return_val = replace(&mut data, &find, &repl);
         assert!(!return_val);
     }
 
@@ -213,7 +213,7 @@ mod tests {
         let mut data: Vec::<u8> = vec![1, 2];
         let find: Vec::<u8> = vec![3, 4, 5, 6];
         let repl: Vec::<u8> = vec![6, 5, 4, 3];
-        let return_val = replace(&mut data, find, repl);
+        let return_val = replace(&mut data, &find, &repl);
         assert!(!return_val);
     }
 }
